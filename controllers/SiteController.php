86c252039d21a0b9db9f8f90c5c8b828392012a9
Andrii Shchur <andriishchur1@gmail.com>
2018-02-05 20:12:30 +0200

    Add simple controller, debug true

---
+++ controllers/SiteController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 05.02.2018
+ * Time: 20:07
+ */
+class SiteController extends \yii\web\Controller{
+    public function actionIndex()
+    {
+        return 'Our CRM';
+    }
+}
--- web/index.php
+++ web/index.php
@@ -5,9 +5,11 @@
  * Date: 05.02.2018
  * Time: 19:57
  */
+define('YII_DEBUG',true);
 //Включаем сам фреймверк
 require(__DIR__.'/../vendor/yiisoft/yii2/Yii.php');
 //Подключаем файл конфирурации
+ini_set('display_errors',true);
 $config = require(__DIR__.'/../config/web.php');
 //Создаем и немедленно запускаем приложение
 (new yii\web\Application($config))->run();

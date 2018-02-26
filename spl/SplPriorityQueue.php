923628c3b919c7de0c3b4056cea861f174c8b3c9
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-26 19:57:55 +0200

    Add spl priority queue

--- spl/SplPriorityQueue.php
+++ spl/SplPriorityQueue.php
@@ -5,3 +5,19 @@
  * Date: 2/26/18
  * Time: 7:48 PM
  */
+
+$queue = new SplPriorityQueue();
+$queue->setExtractFlags(SplPriorityQueue::EXTR_DATA);
+
+$queue->insert('Q', 1);
+$queue->insert('W', 2);
+$queue->insert('E', 6);
+$queue->insert('R', 3);
+$queue->insert('T', 4);
+
+$queue->top();
+
+while ($queue->valid()){
+    echo $queue->current(); //ETRWQ
+    $queue->next();
+}
